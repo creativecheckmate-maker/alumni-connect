445215637ed58360d062c8ab2d4a1e559619e27a
Creative Checkmate <creativecheckmate@gmail.com>
2026-02-21 03:52:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8002433623-f6597",
+  "appId": "1:632211242428:web:83193c5d76434e11d6bfb9",
+  "apiKey": "AIzaSyCICrX9hlYRQPIyZTBBWp5mmq_kNCht2fA",
+  "authDomain": "studio-8002433623-f6597.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "632211242428"
+};
