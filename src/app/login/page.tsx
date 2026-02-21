5a7a8d91ceca02f3293e55854589d55117e2b23c
Creative Checkmate <creativecheckmate@gmail.com>
2026-02-21 03:33:11 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
